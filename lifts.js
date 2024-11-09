80089ccc7b5b748b65cbfa53217c8e3b9f55d57b
saistark1122 <saistark34@gmail.com>
2024-11-09 21:21:39 +0530

    Update lifts.js

--- lifts.js
+++ lifts.js
@@ -11,7 +11,21 @@
             body.classList.remove('light-mode');
         }
     }
+document.addEventListener('DOMContentLoaded', () => {
+    const slides = document.querySelector('.slides');
+    const images = document.querySelectorAll('.slides img');
+    let index = 0;
 
+    function showNextImage() {
+        index++;
+        if (index >= images.length) {
+            index = 0;
+        }
+        slides.style.transform = `translateX(${-index * 100}%)`;
+    }
+
+    setInterval(showNextImage, 3000);
+});
     // Apply the theme when the page loads
     window.onload = applyThemeBasedOnTime;
 
